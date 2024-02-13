3d79314588ce1dca195df2ca466fab91df144fa1
Margarita Slesareva <slesareva.m.a@yandex.ru>
2024-02-13 21:08:16 +0300

    add child vc

--- CompactController/ViewController.swift
+++ CompactController/ViewController.swift
@@ -8,10 +8,36 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    
+    private var childViewController: UIViewController?
+    private let presentButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
+        
+        view.addSubview(presentButton)
+        presentButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            presentButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            presentButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+        ])
+        
+        presentButton.setTitle("Present", for: .normal)
+        presentButton.setTitleColor(.systemBlue, for: .normal)
+        presentButton.addTarget(self, action: #selector(presentButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func presentButtonTapped() {
+//        if childViewController == nil {
+//            
+//        }
+        
+        
+        present(ChildViewController(), animated: true)
+        
+//        addChild(viewController)
     }
 }
